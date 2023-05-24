28edcc9032e2f5d1d2fe4802a7f3a852f1abdef3
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-05-24 11:50:37 -0400

    video code working

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,11 +1,24 @@
 const multer = require("multer");
 const path = require("path");
-
+const videoExtensions = [
+  ".mp4",
+  ".avi",
+  ".mov",
+  ".wmv",
+  ".flv",
+  ".mkv",
+  ".mpg",
+  ".mpeg",
+  ".rm",
+  ".rmvb",
+  ".3gp",
+  ".webm"
+];
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname).toLowerCase();
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && !videoExtensions.includes(ext)) {
       cb(new Error("File type is not supported"), false);
       return;
     }
